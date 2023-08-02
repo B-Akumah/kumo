d771c1a87e97970843b87bb670103de5919ca072
Brian Akumah <bakumah@liberty.edu>
2023-08-02 19:02:39 -0400

    commented all DAO

--- src/main/java/edu/csis/dao/UserDao.java
+++ src/main/java/edu/csis/dao/UserDao.java
@@ -6,10 +6,25 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 /**
+ * Repository interface for User entities, extending JpaRepository for basic CRUD operations.
+ *
  * @author bakumah
  */
-
 public interface UserDao extends JpaRepository<User, Integer> {
+
+    /**
+     * Find all users with the specified username.
+     *
+     * @param username The username to search for.
+     * @return A list of users with the given username.
+     */
     List<User> findAllByUsername(String username);
+
+    /**
+     * Find a single user with the specified username.
+     *
+     * @param username The username to search for.
+     * @return The user with the given username, or null if not found.
+     */
     User findByUsername(String username);
 }
