0a8958ccda53f8985a09b619324dac3c29ee57aa
Brian Akumah <bakumah@liberty.edu>
2023-08-02 19:07:52 -0400

    controller comments

--- src/main/java/edu/csis/controller/DashboardController.java
+++ src/main/java/edu/csis/controller/DashboardController.java
@@ -18,6 +18,9 @@ import java.util.Map.Entry;
 import static javax.swing.JOptionPane.showMessageDialog;
 
 /**
+ * This class represents the Dashboard Controller, responsible for managing the interactions between
+ * the GUI components and the backend services related to the dashboard functionality.
+ *
  * @author bakumah
  */
 public class DashboardController {
@@ -29,6 +32,14 @@ public class DashboardController {
     private final DashboardPage dashboardPage;
     private User user;
 
+    /**
+     * Constructs the DashboardController with necessary dependencies and sets up the dashboard page.
+     *
+     * @param userDao             The UserDao for accessing user-related data.
+     * @param accountDao          The AccountDao for accessing account-related data.
+     * @param fundsTransactionDao The FundsTransactionDao for accessing funds transaction data.
+     * @param loginId             The ID of the logged-in user.
+     */
     public DashboardController(final UserDao userDao, final AccountDao accountDao, final FundsTransactionDao fundsTransactionDao, final int loginId) {
         userDatabaseService = new UserDatabaseService(userDao);
         bankAccountDatabaseService = new BankAccountDatabaseService(accountDao, fundsTransactionDao);
@@ -36,19 +47,24 @@ public class DashboardController {
         dashboardPage = new DashboardPage(user);
         logOutButton = dashboardPage.getLogOutButton();
 
+        // Account Summary Button ActionListener
         dashboardPage.getAccountSummaryButton().addActionListener(asb -> {
+            // Get all bank accounts for the user and show the AccountSummaryPage
             List<Account> bankAccounts = bankAccountDatabaseService.getAllAccountsForUser(user);
             AccountSummaryPage accountSummaryPage = new AccountSummaryPage(bankAccounts);
             dashboardPage.setVisible(false);
 
+            // Account Summary Page Home Button ActionListener
             accountSummaryPage.getHomeButton().addActionListener(hb -> {
                 accountSummaryPage.dispose();
                 dashboardPage.setVisible(true);
             });
 
+            // Set up ActionListeners for transaction buttons on the AccountSummaryPage
             Map<Pair<Integer, Double>, JButton> transactionButtons = accountSummaryPage.getTransactionButtons();
             for (Entry<Pair<Integer, Double>, JButton> buttonEntry : transactionButtons.entrySet()) {
                 buttonEntry.getValue().addActionListener(tb -> {
+                    // Get the selected transaction ID and account balance for the selected account
                     Integer thisTransaction = buttonEntry.getKey().first();
                     Double accountBalance = buttonEntry.getKey().second();
                     List<FundsTransaction> allTransactionsForAccount = bankAccountDatabaseService.getAllTransactionsForAccount(thisTransaction);
@@ -57,6 +73,7 @@ public class DashboardController {
                     dashboardPage.setVisible(false);
                     accountSummaryPage.setVisible(false);
 
+                    // Transaction History Page Back to Summary Button ActionListener
                     transactionHistoryPage.getBackToSummaryButton().addActionListener(btsb -> {
                         transactionHistoryPage.dispose();
                         accountSummaryPage.setVisible(true);
@@ -69,6 +86,7 @@ public class DashboardController {
                             }
                         }
                     });
+                    // Transaction History Page Home Button ActionListener
                     transactionHistoryPage.getHomeButton().addActionListener(hb -> {
                         transactionHistoryPage.dispose();
                         accountSummaryPage.dispose();
@@ -85,11 +103,14 @@ public class DashboardController {
             });
         });
 
+        // Transfer Money Button ActionListener
         dashboardPage.getTransferMoneyButton().addActionListener(tmb -> {
+            // Get all bank accounts for the user and show the TransferFundsPage
             List<Account> bankAccounts = bankAccountDatabaseService.getAllAccountsForUser(user);
             dashboardPage.setVisible(false);
             TransferFundsPage transferFundsPage = new TransferFundsPage(bankAccounts);
 
+            // Transfer Button ActionListener
             transferFundsPage.getTransferButton().addActionListener(tb -> {
                 String transferAmountString = transferFundsPage.getTransferAmountInput().getText();
                 if (transferAmountString == null || transferAmountString.isBlank()) {
@@ -101,19 +122,20 @@ public class DashboardController {
                         int toAccount = transferFundsPage.getTransferTo();
                         double fromAccountBal = transferFundsPage.getFromAccountBalance();
 
+                        // Check if the transfer is valid and perform the transaction
                         if (fromAccount == toAccount) {
                             showMessageDialog(null, "From Account and To Account can NOT be the same");
                         } else if (transferAmount > fromAccountBal) {
-                            showMessageDialog(null, "You can NOT transfer more money that you have");
+                            showMessageDialog(null, "You can NOT transfer more money than you have");
                         } else {
-                            //Transfer the funds
+                            // Transfer the funds
                             boolean successfulTransfer = bankAccountDatabaseService.updateAccountBalance(fromAccount, toAccount, transferAmount);
                             if (successfulTransfer) {
-                                //make the transaction log
+                                // Log the transaction
                                 if (bankAccountDatabaseService.logTransaction(fromAccount, toAccount, transferAmount)) {
                                     showMessageDialog(null, "Funds Successfully Transferred, and Transaction has been successfully logged");
                                 } else {
-                                    showMessageDialog(null, "The funds were transferred however something went wrong with logging the transaction");
+                                    showMessageDialog(null, "The funds were transferred; however, something went wrong with logging the transaction");
                                 }
                                 transferFundsPage.dispose();
                                 dashboardPage.setVisible(true);
@@ -129,12 +151,14 @@ public class DashboardController {
 
             });
 
+            // Transfer Funds Page Cancel Button ActionListener
             transferFundsPage.getCancelButton().addActionListener(cb -> {
                 transferFundsPage.dispose();
                 dashboardPage.setVisible(true);
 
             });
 
+            // Transfer Funds Page Home Button ActionListener
             transferFundsPage.getHomeButton().addActionListener(hb -> {
                 transferFundsPage.dispose();
                 dashboardPage.setVisible(true);
@@ -147,15 +171,19 @@ public class DashboardController {
             });
         });
 
+        // Account Manager Button ActionListener
         dashboardPage.getAccountManagerButton().addActionListener(amb -> {
             dashboardPage.setVisible(false);
             AccountManagerPage accountManagerPage = new AccountManagerPage();
 
+            // Account Manager Page Edit User Info Button ActionListener
             accountManagerPage.getEditUserInfoButton().addActionListener(euib -> {
                 accountManagerPage.setVisible(false);
                 EditUserPage editUserPage = new EditUserPage(user);
 
+                // Edit User Page Save Button ActionListener
                 editUserPage.getSaveButton().addActionListener(sb -> {
+                    // Get updated user info from the EditUserPage and update the user object
                     String firstName = editUserPage.getFirstNameInput().getText();
                     String lastName = editUserPage.getLastNameInput().getText();
                     String email = editUserPage.getEmailInput().getText();
@@ -180,6 +208,7 @@ public class DashboardController {
                     address.setZipCode(zipCode);
                     user.setAddress(address);
 
+                    // Update the user info in the database
                     if (userDatabaseService.updateUser(user)) {
                         showMessageDialog(null, "User successfully updated");
                         dashboardPage.setVisible(false);
@@ -189,10 +218,13 @@ public class DashboardController {
                     }
                 });
 
+                // Edit User Page Account Manager Button ActionListener
                 editUserPage.getAccountManagerButton().addActionListener(amb2 -> {
                     accountManagerPage.setVisible(true);
                     editUserPage.dispose();
                 });
+
+                // Edit User Page Home Button ActionListener
                 editUserPage.getHomeButton().addActionListener(hb -> {
                     dashboardPage.setVisible(true);
                     editUserPage.dispose();
@@ -208,10 +240,12 @@ public class DashboardController {
                 });
             });
 
+            // Account Manager Page Add New Account Button ActionListener
             accountManagerPage.getAddNewAccountButton().addActionListener(anab -> {
                 accountManagerPage.setVisible(false);
                 AddAccountPage addAccountPage = new AddAccountPage();
 
+                // Add Account Page Create Button ActionListener
                 addAccountPage.getCreateButton().addActionListener(cb -> {
                     String startingBalanceText = addAccountPage.getStartingBalanceInput().getText();
                     if (startingBalanceText == null || startingBalanceText.isBlank()) {
@@ -226,7 +260,7 @@ public class DashboardController {
                                 case SAVINGS -> {
                                     boolean savingAccount = bankAccountDatabaseService.createSavingAccount(user, startingBalance);
                                     if (!savingAccount) {
-                                        showMessageDialog(null, "Error creating Savings account. (Remember there is a max of 5 account total)");
+                                        showMessageDialog(null, "Error creating Savings account. (Remember there is a max of 5 accounts in total)");
                                     }
                                     accountManagerPage.setVisible(true);
                                     addAccountPage.dispose();
@@ -235,7 +269,7 @@ public class DashboardController {
                                 case CHECKING -> {
                                     boolean checkingAccount = bankAccountDatabaseService.createCheckingAccount(user, startingBalance);
                                     if (!checkingAccount) {
-                                        showMessageDialog(null, "Error creating Checking account. (Remember there is a max of 5 account total)");
+                                        showMessageDialog(null, "Error creating Checking account. (Remember there is a max of 5 accounts in total)");
                                     }
                                     accountManagerPage.setVisible(true);
                                     addAccountPage.dispose();
@@ -249,10 +283,13 @@ public class DashboardController {
                     }
                 });
 
+                // Add Account Page Account Manager Button ActionListener
                 addAccountPage.getAccountManagerButton().addActionListener(amb2 -> {
                     accountManagerPage.setVisible(true);
                     addAccountPage.dispose();
                 });
+
+                // Add Account Page Home Button ActionListener
                 addAccountPage.getHomeButton().addActionListener(hb -> {
                     addAccountPage.dispose();
                     accountManagerPage.dispose();
@@ -268,6 +305,7 @@ public class DashboardController {
                 });
             });
 
+            // Account Manager Page Home Button ActionListener
             accountManagerPage.getHomeButton().addActionListener(hb -> {
                 dashboardPage.setVisible(true);
                 accountManagerPage.dispose();
@@ -279,7 +317,6 @@ public class DashboardController {
                 }
             });
         });
-
     }
 
     public DashboardPage getDashboardPage() {
--- src/main/java/edu/csis/controller/LoginController.java
+++ src/main/java/edu/csis/controller/LoginController.java
@@ -7,7 +7,6 @@ import edu.csis.dao.UserDao;
 import edu.csis.model.Address;
 import edu.csis.model.User;
 import edu.csis.services.BankAccountDatabaseService;
-import edu.csis.services.DummyDataService;
 import edu.csis.services.UserDatabaseService;
 import edu.csis.view.CreateAccountPage;
 import edu.csis.view.LoginPage;
@@ -20,6 +19,9 @@ import java.awt.event.WindowEvent;
 import static javax.swing.JOptionPane.showMessageDialog;
 
 /**
+ * This class represents the Login Controller, responsible for managing the interactions between
+ * the GUI components and the backend services related to user login and account creation.
+ *
  * @author bakumah
  */
 @Controller
@@ -31,36 +33,54 @@ public class LoginController {
     private final FundsTransactionDao fundsTransactionDao;
     boolean isLoggedIn = false;
 
+    /**
+     * Constructs the LoginController with necessary dependencies.
+     *
+     * @param userDao             The UserDao for accessing user-related data.
+     * @param accountDao          The AccountDao for accessing account-related data.
+     * @param fundsTransactionDao The FundsTransactionDao for accessing funds transaction data.
+     */
     public LoginController(UserDao userDao, AccountDao accountDao, FundsTransactionDao fundsTransactionDao) {
         userDatabaseService = new UserDatabaseService(userDao);
-        bankAccountDatabaseService = new BankAccountDatabaseService(accountDao,fundsTransactionDao);
+        bankAccountDatabaseService = new BankAccountDatabaseService(accountDao, fundsTransactionDao);
         this.userDao = userDao;
         this.accountDao = accountDao;
         this.fundsTransactionDao = fundsTransactionDao;
     }
 
+    /**
+     * Starts the Kumo application, setting up the main page and its listeners.
+     */
     public void startKumo() {
-
+        // Main Page setup
         MainPage mainPage = new MainPage();
+
+        // Login Button ActionListener
         mainPage.getLoginButton().addActionListener(lp -> {
+            // Login Page setup
             LoginPage loginPage = new LoginPage();
             mainPage.setVisible(false);
 
+            // Login Button ActionListener
             loginPage.getLoginButton().addActionListener(lb -> {
                 String username = loginPage.getUsernameInput().getText();
                 char[] password = loginPage.getPasswordInput().getPassword();
 
+                // Check if the required fields are populated
                 if (username == null || username.isBlank() || password == null || password.length == 0) {
                     showMessageDialog(null, "All Fields must be populated");
                 } else {
+                    // Verify login credentials
                     int login = userDatabaseService.verifyLogin(username, password);
 
                     if (login > 0) {
+                        // If login is successful, show the DashboardController and set the user as logged in
                         DashboardController dashboardController = new DashboardController(userDao, accountDao, fundsTransactionDao, login);
                         isLoggedIn = true;
                         loginPage.dispose();
                         mainPage.setVisible(false);
 
+                        // Logout Button ActionListener for the DashboardController
                         dashboardController.getLogOutButton().addActionListener(lob -> {
                             isLoggedIn = false;
                             dashboardController.setUser(null);
@@ -68,10 +88,13 @@ public class LoginController {
                             mainPage.setVisible(true);
                         });
                     } else {
+                        // Show an error message for invalid login
                         showMessageDialog(null, "Invalid login. Try again.");
                     }
                 }
             });
+
+            // Login Page WindowListener
             loginPage.addWindowListener(new WindowAdapter() {
                 @Override
                 public void windowClosed(WindowEvent e) {
@@ -81,10 +104,16 @@ public class LoginController {
                 }
             });
         });
+
+        // Create Account Button ActionListener
         mainPage.getCreateAccountButton().addActionListener(cab -> {
+            // Create Account Page setup
             CreateAccountPage createAccountPage = new CreateAccountPage();
             mainPage.setVisible(false);
+
+            // Create Button ActionListener
             createAccountPage.getCreateButton().addActionListener(cb -> {
+                // Retrieve input values from the Create Account Page
                 String firstName = createAccountPage.getFirstNameInput().getText();
                 String lastName = createAccountPage.getLastNameInput().getText();
                 String username = createAccountPage.getUsernameInput().getText();
@@ -98,6 +127,7 @@ public class LoginController {
                 String nation = createAccountPage.getNation();
                 String zipCode = createAccountPage.getZipCodeInput().getText();
 
+                // Create an Address object using the input values
                 Address address = Address.builder()
                         .streetLine1(address1)
                         .streetLine2(address2)
@@ -109,7 +139,8 @@ public class LoginController {
 
                 char[] password = createAccountPage.getPasswordInput().getPassword();
 
-                if (firstName == null || lastName == null || username == null || email == null || phone == null || password == null || address1 == null ||  city == null ||   state == null ||
+                // Check if all required fields are populated
+                if (firstName == null || lastName == null || username == null || email == null || phone == null || password == null || address1 == null || city == null || state == null ||
                         firstName.isBlank() || lastName.isBlank() || username.isBlank() || email.isBlank() || phone.isBlank() || password.length == 0 || address1.isBlank() || city.isBlank() || state.isBlank()) {
                     showMessageDialog(null, "All Fields must be populated (Except Address Line 2)");
                 } else {
@@ -124,12 +155,13 @@ public class LoginController {
                             // Open initial bank accounts
                             bankAccountDatabaseService.createCheckingAccount(createdUser);
                             bankAccountDatabaseService.createSavingAccount(createdUser);
-                            // Show them Dashboard Screen
+
+                            // Show the Dashboard Screen
                             DashboardController dashboardController = new DashboardController(userDao, accountDao, fundsTransactionDao, createdUser.getUserID());
-                            // dispose create account pane
                             mainPage.setVisible(false);
                             createAccountPage.dispose();
 
+                            // Logout Button ActionListener for the DashboardController
                             dashboardController.getLogOutButton().addActionListener(lob -> {
                                 isLoggedIn = false;
                                 dashboardController.setUser(null);
@@ -144,6 +176,8 @@ public class LoginController {
                     }
                 }
             });
+
+            // Create Account Page WindowListener
             createAccountPage.addWindowListener(new WindowAdapter() {
                 @Override
                 public void windowClosed(WindowEvent e) {
@@ -155,3 +189,4 @@ public class LoginController {
         });
     }
 }
+
