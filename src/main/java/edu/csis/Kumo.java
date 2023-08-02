155b37aad255eaf7e81d2027e424e73e89295789
Brian Akumah <bakumah@liberty.edu>
2023-08-02 19:26:28 -0400

    comments done

--- src/main/java/edu/csis/Kumo.java
+++ src/main/java/edu/csis/Kumo.java
@@ -7,7 +7,6 @@ import edu.csis.dao.UserDao;
 import edu.csis.services.DummyDataService;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -15,23 +14,36 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import java.awt.*;
 
 /**
+ * The main class for the KUMO Banking application.
+ *
  * @author bakumah
  */
-@EntityScan(basePackages = {"edu.csis.*"})
+@SpringBootApplication
 @EnableJpaRepositories(basePackages = {"edu.csis.*"})
-@EnableTransactionManagement
-@SpringBootApplication()
-//@ComponentScan(basePackages = "edu.csis.*")
+// Enable JPA repositories scanning for the specified base packages
+@EnableTransactionManagement // Enable Spring transaction management
 public class Kumo {
+
+    /**
+     * The entry point of the KUMO Banking application.
+     *
+     * @param args The command-line arguments passed to the application.
+     */
     public static void main(String[] args) {
-        var appContext = new SpringApplicationBuilder(Kumo.class)
-                .headless(false).web(WebApplicationType.NONE).run(args);
+        // Create the Spring application context with non-headless and non-web configurations
+        var appContext = new SpringApplicationBuilder(Kumo.class).headless(false).web(WebApplicationType.NONE).run(args);
+
+        // Get instances of the necessary data access objects from the Spring application context
         UserDao userDao = appContext.getBean(UserDao.class);
         AccountDao accountDao = appContext.getBean(AccountDao.class);
         FundsTransactionDao fundsTransactionDao = appContext.getBean(FundsTransactionDao.class);
+
+        // Populate dummy data into the database using the DummyDataService
+        // and start the KUMO Banking application by launching the LoginController
         EventQueue.invokeLater(() -> {
             new DummyDataService(userDao, accountDao, fundsTransactionDao).populateDummyData();
             new LoginController(userDao, accountDao, fundsTransactionDao).startKumo();
         });
     }
 }
+
